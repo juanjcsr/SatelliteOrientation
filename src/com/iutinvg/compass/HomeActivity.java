1fb4dc0ea28e4d7337f59aaaffb848d65a720317
Juan Carlos <juanjcsr@gmail.com>
2014-04-12 17:12:30 -0500

    nueva version con imagen

---
+++ src/com/iutinvg/compass/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.iutinvg.compass;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by JuanCarlos on 4/12/14.
+ */
+public class HomeActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
